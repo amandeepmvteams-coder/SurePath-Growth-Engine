91da322f8c209d73d3819d5759a96336119b576e
Amandeep Rawat <amandeep.rawat@myvirtualTeams.com>
2026-09-21 09:10:57 +0530

    Production Changes Implement at environment variable

--- surepath-frontend/src/lib/api/client.ts
+++ surepath-frontend/src/lib/api/client.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: process.env.NEXT_BACKEND_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
-        "X-API-Key": process.env.NEXT_API_ACCESS_KEY,
+        "X-API-Key": process.env.NEXT_PUBLIC_API_KEY,
     },
 });
