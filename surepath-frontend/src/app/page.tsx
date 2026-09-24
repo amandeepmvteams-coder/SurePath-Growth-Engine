ba474998be5b6bd3ba2cec5abed125d522564599
Amandeep Rawat <amandeep.rawat@myvirtualTeams.com>
2026-09-24 17:27:10 +0530

    sidebar logo navigation  issue fixed

--- surepath-frontend/src/app/page.tsx
+++ surepath-frontend/src/app/page.tsx
@@ -1,5 +1,14 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default function Page() {
-      redirect("/login");
+export default async function Page() {
+    const cookieStore = await cookies();
+
+    const sessionToken = cookieStore.get("surepath_session");
+
+    if (sessionToken?.value) {
+        redirect("/dashboard");
+    }
+
+    redirect("/login");
 }
